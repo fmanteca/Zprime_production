6546f0dd85d5f73bc1c6ee96ae7346536be28a14
fmanteca <fmanteca@ifca.unican.es>
2020-07-10 11:38:52 +0200

    updated

--- crab_cfg_step2.py
+++ crab_cfg_step2.py
@@ -3,20 +3,20 @@ config = config()
 
 config.section_("General")
 config.General.transferLogs = True
-config.General.requestName = 'ZprimeToMuMu5000_GEN_SIM_PREMIX' 
+config.General.requestName = 'ZprimeToMuMu5000_GEN_SIM_PREMIX_RECO' 
 config.General.workArea = 'crab_projects'
 
 
 config.section_("JobType")
 config.JobType.pluginName  = 'Analysis'
-config.JobType.psetName = 'step1_premix.py'
+config.JobType.psetName = 'step2_RECO.py'
 config.JobType.numCores = 8
 config.JobType.maxMemoryMB = 10000
 
 config.section_("Data")
 config.Data.splitting       = 'FileBased'
 config.Data.unitsPerJob = 1
-config.Data.outputDatasetTag = 'ZprimeToMuMu5000_GEN_SIM_PREMIX'
+config.Data.outputDatasetTag = 'ZprimeToMuMu5000_GEN_SIM_PREMIX_RECO'
 config.Data.inputDBS = 'phys03'
 config.Data.outLFNDirBase = '/store/user/fernanpe/'
 config.Data.publication = True
